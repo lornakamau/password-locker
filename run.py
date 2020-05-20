e1b8f720041ca447272d10734958277731a14f24
Lorna Kamau <kamaulorna@gmail.com>
2020-05-20 17:20:05 +0300

    create a function that checks if a credential exists and on to create a credential

--- run.py
+++ run.py
@@ -17,3 +17,17 @@ def find_user(username):
 
 def check_existing_user(username):
     return Users.user_exists(username)
+
+def create_credentials(application_name, account_username, account_password):
+    new_credentials = Credentials(application_name, account_username, account_password)
+    return new_credentials
+
+def display_credentials():
+    return Credentials.display_credentials()
+
+def credentials_exist(application_name):
+    return Credentials.credentials_exist(application_name)
+    
+def main():
+    print("Hello Welcome to Password Locker.")
+    print("Sign up")
