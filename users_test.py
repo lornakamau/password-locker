1512428e4fcc6226a899fb11ea43de2cb1addff2
Lorna Kamau <kamaulorna@gmail.com>
2020-05-20 14:10:22 +0300

    add test to check that a user is added correctly

--- users_test.py
+++ users_test.py
@@ -9,3 +9,7 @@ class TestUsers(unittest.TestCase):
     def test_initialization(self):
         self.assertEqual(self.new_user.username, "Lorna")
         self.assertEqual(self.new_user.login_password, "97")
+
+    def test_add_user(self):
+        self.new_user.add_user()
+        self.assertEqual(len(Users.users_list), 1)
