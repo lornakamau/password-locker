c1acd05652eb2a5cca3154f2205476237e72dbbd
Lorna Kamau <kamaulorna@gmail.com>
2020-05-20 14:08:04 +0300

    add a function to add users

--- users_test.py
+++ users_test.py
@@ -4,3 +4,6 @@ class Users:
     def __init__ (self, username, login_password):
         self.username = username
         self.login_password = login_password
+    
+    def add_user(self):
+        Users.users_list.append(self)
