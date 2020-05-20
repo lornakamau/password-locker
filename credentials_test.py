8ff82e48a1a8f8e0f3a7d2163e1e79ddfe372bca
Lorna Kamau <kamaulorna@gmail.com>
2020-05-20 14:43:46 +0300

    test that multiple credentials can be added

--- credentials_test.py
+++ credentials_test.py
@@ -10,3 +10,12 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.application_name, "Twitter")
         self.assertEqual(self.new_credentials.account_username, "cirukamau")
         self.assertEqual(self.new_credentials.account_password, "lol")
+
+    def tearDown(self):
+        Credentials.credentials_list = []
+
+    def test_add_multiple_users(self):
+        self.new_credentials.add_credentials()
+        test_credentials = Credentials("Instagram", "Alfred", "101")
+        test_credentials.add_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 2)
