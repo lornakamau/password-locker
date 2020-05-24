8f9fb17eccf1ec0799cb11d39c921db47db52c05
Lorna Kamau <kamaulorna@gmail.com>
2020-05-24 11:06:15 +0300

    add docstrings to credentials_test.py

--- credentials_test.py
+++ credentials_test.py
@@ -1,26 +1,47 @@
-import unittest
+import unittest # Importing the unittest module
 from credentials import Credentials
 
 class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines test cases for Credentials class behaviours
+    
+    Args:
+        unittest.TestCase: TestCase class that  helps in creating test cases
+    '''
     
     def setUp(self):
+        '''
+        Set up method to run before each test cases
+        '''
         self.new_credentials = Credentials("Twitter", "cirukamau", "lol")
 
     def test_initialization(self):
+        '''
+        Test initialization test case to test if the object is initialized properly
+        '''
         self.assertEqual(self.new_credentials.application_name, "Twitter")
         self.assertEqual(self.new_credentials.account_username, "cirukamau")
         self.assertEqual(self.new_credentials.account_password, "lol")
 
     def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run
+        '''
         Credentials.credentials_list = []
 
     def test_add_multiple_credentials(self):
+        '''
+        test_add_multiple_credentials to check if we can save multiple credential objects to our credentials list
+        '''
         self.new_credentials.add_credentials()
         test_credentials = Credentials("Instagram", "Alfred", "101")
         test_credentials.add_credentials()
         self.assertEqual(len(Credentials.credentials_list), 2)
 
     def test_delete_credentials(self):
+        '''
+        test_delete_credentials to test if we can remove a credential from credentials list
+        '''
         self.new_credentials.add_credentials()
         test_credentials = Credentials("Instagram", "Alfred", "101")
         test_credentials.add_credentials()
@@ -29,9 +50,15 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(len(Credentials.credentials_list), 1)
 
     def test_display_all_credentials(self):
+        '''
+        test method that returns a list of all credentials saved
+        '''
         self.assertEqual(Credentials.display_credentials(), Credentials.credentials_list)
 
     def test_find_by_application_name(self):
+        '''
+        test to check if we can find a credentials by application name
+        '''
         self.new_credentials.add_credentials()
         test_credentials = Credentials("Instagram", "Alfred", "101")
         test_credentials.add_credentials()
@@ -40,6 +67,9 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(found_credential.account_username, test_credentials.account_username)
         
     def test_credentials_exist(self):
+        ''' 
+        test to check if we can return boolean if credential exist or not
+        '''
         self.new_credentials.add_credentials()
         test_credentials = Credentials("Instagram", "Alfred", "101")
         test_credentials.add_credentials()
